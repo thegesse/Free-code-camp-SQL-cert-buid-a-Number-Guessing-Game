a841f23799e212960af87ef1a7c2f50cb515be03
Esteban Quillerier <emquillerier@gmail.com>
2026-09-22 14:46:42 -0400

    feat: add number guessing logic

--- number_guess.sh
+++ number_guess.sh
@@ -6,6 +6,7 @@ SECRET_NUM=$(( RANDOM % 1000 + 1))
 echo "Enter your username:"
 read USERNAME
 
+echo "$SECRET_NUM"
 USER_DATA=$($PSQL "SELECT user_id, games_played, best_game FROM users WHERE username='$USERNAME';")
 
 if [[ -z $USER_DATA ]]; then
@@ -25,4 +26,19 @@ fi
 NUMBER_OF_GUESSES=0
 echo "Guess the secret number between 1 and 1000:"
 read GUESS
+
+while true
+do
+  ((NUMBER_OF_GUESSES++))
+  if [[ ! $GUESS =~ ^[0-9]+$ ]]; then
+    echo "That is not a integer, guess again:"
+  elif  [[ $GUESS -eq $SECRET_NUM ]]; then
+    break
+  elif [[ $GUESS -gt $SECRET_NUM ]]; then
+    echo "It's lower than that, guess again:"
+  else
+    echo "It's higher than that, guess again:"
+  fi
+  read GUESS
+done
 
