90819605c15a0d34371f4c8966c8ca04b397a107
Esteban Quillerier <emquillerier@gmail.com>
2026-09-22 14:39:06 -0400

    feat: added username recognition

--- number_guess.sh
+++ number_guess.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
-NUM=$RANDOM
-echo "$NUM"
+SECRET_NUM=$(( RANDOM % 1000 + 1))
+
+echo "Enter your username:"
+read USERNAME
+
+USER_DATA=$($PSQL "SELECT user_id, games_played, best_game FROM users WHERE username='$USERNAME';")
+
+if [[ -z $USER_DATA ]]; then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  INSERT_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME');")
+  GAMES_PLAYED=0
+  BEST_GAME=0
+else
+  IFS="|" read USER_ID GAMES_PLAYED BEST_GAME <<< "$USER_DATA"
+  GAMES_PLAYED=$(echo "$GAMES_PLAYED" | xargs)
+  BEST_GAME=$(echo "$BEST_GAME" | xargs)
+
+
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
+
+NUMBER_OF_GUESSES=0
+echo "Guess the secret number between 1 and 1000:"
+read GUESS
 
